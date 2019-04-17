60167770436042b13e5bd21da9d5e6a5e4bf98e1
Santiago <santiagoperaza@icloud.com>
2019-04-17 19:23:11 -0300

    adaptacion menu para importarlo desde reg-log

--- menu.py
+++ menu.py
@@ -1,32 +1,50 @@
-import tkinter as tk
-
-root = tk.Tk()
-
-
-root.title("BitApp") 
-root.resizable(0,0) 
-
-root.geometry('1024x900') 
+from tkinter import *
+# import time
 
 
-frame = tk.Frame(root, width=1024, height=900)
+def pedido():
+    pedido= Toplevel(menu)
+    pedido.resizable(0,0) 
+    pedido.geometry('1024x900') 
 
-boton = tk.Button(frame, text="Pedido",width=30,height = 2)
-boton.place(relx=0.5, rely=0.3, anchor=tk.CENTER)
-# boton.pack()
-boton2 = tk.Button(frame, text="Contraindicaciones",width=30,height = 2)
-boton2.place(relx=0.5, rely=0.4, anchor=tk.CENTER)
+    pedido.focus_set()
+    pedido.grab_set()
+    pedido.wait_window(pedido)
 
-boton3 = tk.Button(frame, text="Dosis",width=30,height = 2)
-boton3.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
 
-boton3 = tk.Button(frame, text="Historial",width=30,height = 2)
-boton3.place(relx=0.5, rely=0.6, anchor=tk.CENTER)
+    
 
-boton3 = tk.Button(frame, text="Vencimientos",width=30,height = 2)
-boton3.place(relx=0.5, rely=0.7, anchor=tk.CENTER)
 
-frame.pack(side="top", anchor="w") #Empaqueta el elemento en la raíz si no, no se puede mostrar, paramestros para posición
+
+
+def menu():
+    global menu
+    menu = Tk()
+    menu.title("BitApp") 
+    menu.resizable(0,0) 
+    menu.geometry('1024x900') 
+
+
+    frame = Frame(menu, width=1024, height=900)
+
+    boton1 = Button(frame, text="Pedido",width=30,height = 2, command = pedido)
+    boton1.place(relx=0.5, rely=0.3, anchor=CENTER)
+
+    boton2 = Button(frame, text="Contraindicaciones",width=30,height = 2)
+    boton2.place(relx=0.5, rely=0.4, anchor=CENTER)
+
+    boton3 = Button(frame, text="Dosis",width=30,height = 2)
+    boton3.place(relx=0.5, rely=0.5, anchor=CENTER)
+
+    boton4 = Button(frame, text="Historial",width=30,height = 2)
+    boton4.place(relx=0.5, rely=0.6, anchor=CENTER)
+
+    boton5 = Button(frame, text="Vencimientos",width=30,height = 2)
+    boton5.place(relx=0.5, rely=0.7, anchor=CENTER)
+
+
+
+    frame.pack(side="top", anchor="w") #Empaqueta el elemento en la raíz si no, no se puede mostrar, paramestros para posición
 
 #Loop principal
-root.mainloop() #ciclo para que inicien los procesos y capte los eventos
+# root.mainloop() #ciclo para que inicien los procesos y capte los eventos
